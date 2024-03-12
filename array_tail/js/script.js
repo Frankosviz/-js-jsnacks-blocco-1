954335bc29e0707ddccc1abe021a3d7cba2084cd
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-12 16:43:34 +0100

    array tail start

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,8 +1,31 @@
-2. La coda dell'Array
-Cartella: array_tail
-Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
-Stampiamo poi gli ultimi 5 elementi dell'Array
-Bonus:
-Chiediamo all'utente quanti elementi dell'array dovremo stampare
-Suggerimenti:
-Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+// 2. La coda dell'Array
+// Cartella: array_tail
+// Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
+// Stampiamo poi gli ultimi 5 elementi dell'Array
+// Bonus:
+// Chiediamo all'utente quanti elementi dell'array dovremo stampare
+// Suggerimenti:
+// Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+
+// Svolgimento..
+
+// Associamo tutto ad un bottone
+
+const button = document.querySelector('.btn');
+
+// Al click succederà che
+
+button.addEventListener('click', function(){
+    const nElement = parseInt(document.getElementById('userValue').value);
+    
+    // Otteniamo il valore numerico che l'utente ha scelto
+    console.log(nElement); 
+
+    // Controlliamo se i numeri sono minori o uguali a 0
+    
+    if (isNaN(nElement) || nElementi <= 0) {
+        alert("Numero rifiutato. Inserisci un numero valido");
+
+        
+})
+
